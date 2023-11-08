a2acdcd6ce15fbb52913192c6030b4ac4bfd1022
Pasu Sangiemsin <opxz7148@gmail.com>
2023-11-08 22:11:25 +0700

    Complete combination_gen function

---
+++ combination_gen.py
@@ -0,0 +1,33 @@
+from copy import deepcopy
+def gen_comb_list(lists):
+    result = []
+
+    check_val = 1
+
+    for ls in lists:
+        if len(ls) != 1:
+            check_val *= -1
+            break
+
+    if check_val > 0:
+        temp = []
+        for x in lists:
+            temp.append(x[0])
+        return temp
+    else:
+        for i in range(len(lists)):
+            if len(lists[i]) != 1:
+                for j in lists[i]:
+                    copy_lsts = deepcopy(lists)
+                    copy_lsts[i] = [j]
+                    new_comb = gen_comb_list(copy_lsts)
+                    if isinstance((new_comb[0]), int):
+                        result.append(new_comb)
+                    else:
+                        result += new_comb
+                return result
+
+ls = [[1,2,3], [4,5], [6, 7, 8]]
+
+print(gen_comb_list(ls))
+
